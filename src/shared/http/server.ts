275a0fff091308bfe509a27bcf1811bfe82c59d5
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-10 21:46:44 -0300

    refactor: :recycle: remove get route on the server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -10,10 +10,6 @@ app.use(express.json());
 
 app.use(routes);
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello' });
-});
-
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
