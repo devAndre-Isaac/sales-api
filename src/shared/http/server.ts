9ff0b5cfaec549372c7c592b6971ce394164a19c
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-11 19:53:19 -0300

    feat: :sparkles: add connection in server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Response, Request } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
