93edcd60b0bc85bfa88e16d30745b4b81eed6326
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-16 20:05:36 -0300

    feat: :sparkles: add controller for update

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import CreateProductService from '../services/CreateProductService';
 import ListProductService from '../services/ListProductService';
 import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductDervice';
 
 export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -28,6 +29,21 @@ export default class ProductsController {
     const createProduct = new CreateProductService();
 
     const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
+  }
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+    const { id } = request.params;
+
+    const updateProduct = new UpdateProductService();
+
+    const product = await updateProduct.execute({
+      id,
       name,
       price,
       quantity,
