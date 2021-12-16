bbb107375fed6de18f87dfab589710f7aeb4b655
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-16 20:00:19 -0300

    feat: :sparkles: add controller for showProduct

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
 
 export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -8,5 +9,15 @@ export default class ProductsController {
     const products = await listProducts.execute();
 
     return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showProduct = new ShowProductService();
+
+    const product = showProduct.execute({ id });
+
+    return response.json(product);
   }
 }
