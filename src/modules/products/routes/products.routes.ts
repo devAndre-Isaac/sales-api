d0e08309f4077b19078a7285d00091eb24468b58
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-17 17:45:13 -0300

    feat: :sparkles: add rouite for create a product

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -6,3 +6,4 @@ const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.index);
 productsRouter.get('/:id', productsController.show);
+productsRouter.post('/', productsController.create);
