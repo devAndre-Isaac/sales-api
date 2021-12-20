21137e246a589b074622be6675a581ed184e4e37
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-20 20:03:09 -0300

    feat: :sparkles: add route for create user

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -7,3 +7,15 @@ const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.get('/', usersController.index);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
