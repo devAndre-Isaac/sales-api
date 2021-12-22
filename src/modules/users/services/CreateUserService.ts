e6538dd8ad1a3065b9f07c7d7db2702cc4947d83
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-21 21:00:22 -0300

    feat: :sparkles: add hashed password for CreateUserService

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/enitites/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -13,14 +14,19 @@ class CreateUserService {
   public async execute({ name, email, password }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
     const emailExists = await usersRepository.findByEmail(email);
+
     if (emailExists) {
       throw new AppError('Email address already used');
     }
+
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
+
     await usersRepository.save(user);
     return user;
   }
