0c26c2bb83632b767aef9d968723ed1e18eab89e
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-23 20:32:31 -0300

    feat: :sparkles: add request.use for see id in request

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Response, Request } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -18,6 +24,13 @@ export default function isAuthenticated(
 
   try {
     const decodeToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodeToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch {
     throw new AppError('Invalid JWT Token');
