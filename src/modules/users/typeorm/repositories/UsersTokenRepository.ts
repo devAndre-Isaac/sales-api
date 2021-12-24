59e6f8ce33b7673bd1d3d9a93ec709fb1ff8c659
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-24 16:51:22 -0300

    feat: :sparkles: add UsersTokenRepository for findByToken

---
+++ src/modules/users/typeorm/repositories/UsersTokenRepository.ts
@@ -0,0 +1,16 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UsersTokenRepository extends Repository<UserToken> {
+  public async findByName(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+    return userToken;
+  }
+}
+
+export default UsersTokenRepository;
