3bcea210da1935f5973fa3a578f3d9ddd836de18
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-23 21:48:55 -0300

    fix: :bug: fix principal routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,7 +4,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
-routes.use('products', productsRouter);
-routes.use('users', usersRouter);
+routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
