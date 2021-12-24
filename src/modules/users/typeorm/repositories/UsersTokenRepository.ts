21f7f54f05f2c6a7c3137a7f75583021d253780b
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-24 16:51:36 -0300

    refactor: :recycle: add findByToken

--- src/modules/users/typeorm/repositories/UsersTokenRepository.ts
+++ src/modules/users/typeorm/repositories/UsersTokenRepository.ts
@@ -3,7 +3,7 @@ import UserToken from '../entities/UserToken';
 
 @EntityRepository(UserToken)
 class UsersTokenRepository extends Repository<UserToken> {
-  public async findByName(token: string): Promise<UserToken | undefined> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
     const userToken = await this.findOne({
       where: {
         token,
