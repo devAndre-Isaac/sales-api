084e2d6309c8d585e30ba6bf4c7fe3aa0a51387e
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-25 00:18:02 -0300

    refactor: :recycle: remove unused imports

--- src/modules/users/services/SendForgotPasswordEmailService.ts.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts.ts
@@ -1,6 +1,5 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UsersTokenRepository from '../typeorm/repositories/UsersTokenRepository';
 
