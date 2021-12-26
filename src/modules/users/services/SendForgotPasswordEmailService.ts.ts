df1f2a545fec09d902540ac7ead57dd10b35445e
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-26 18:06:23 -0300

    feat: :sparkles: add EtherealMail in SendForgotPasswordEmailService

--- src/modules/users/services/SendForgotPasswordEmailService.ts.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UsersTokenRepository from '../typeorm/repositories/UsersTokenRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -18,6 +19,10 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
     const token = await userTokenRepository.generate(user.id);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 
