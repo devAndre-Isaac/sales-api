290a13ed1199b220ccae3ccfa964d6106831a899
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-30 19:18:02 -0300

    feat: :sparkles: add listCustomers controller

---
+++ src/modules/customers/controllers/CustomersController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import ListCustomerService from '../services/ListCustomerService';
+
+export default class CustomersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    const customers = await listCustomers.execute();
+
+    return response.json(customers);
+  }
+}
