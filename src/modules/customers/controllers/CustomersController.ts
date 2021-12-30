53a23f2543b728a4cbd1c842fd483171552ca08a
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-30 19:19:32 -0300

    feat: :sparkles: add showCustomer controller

--- src/modules/customers/controllers/CustomersController.ts
+++ src/modules/customers/controllers/CustomersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import ListCustomerService from '../services/ListCustomerService';
+import ShowCustomerService from '../services/ShowCustomerService';
 
 export default class CustomersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -8,5 +9,14 @@ export default class CustomersController {
     const customers = await listCustomers.execute();
 
     return response.json(customers);
+  }
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showCustomer = new ShowCustomerService();
+
+    const customer = await showCustomer.execute({ id });
+
+    return response.json(customer);
   }
 }
