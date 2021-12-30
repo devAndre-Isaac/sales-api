68566f3cd8df87b77dc050b938aa4eed063e9f85
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-30 19:25:31 -0300

    feat: :sparkles: insert customersRouter in server

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
