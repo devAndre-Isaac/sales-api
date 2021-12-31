5cd8e4423528434397bc99ce870b074aa99ea822
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-31 20:54:15 -0300

    feat: :sparkles: add createOrder Repository

--- src/modules/orders/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -1,5 +1,16 @@
+import Customer from '@modules/customers/typeorm/entities/Customer';
 import { EntityRepository, Repository } from 'typeorm';
 import Order from '../entities/Order';
+
+interface IProduct {
+  product_id: string;
+  preco: number;
+  quantity: number;
+}
+interface IRequest {
+  customer: Customer;
+  products: IProduct[];
+}
 
 @EntityRepository(Order)
 export class OrdersRepository extends Repository<Order> {
@@ -7,6 +18,16 @@ export class OrdersRepository extends Repository<Order> {
     const order = this.findOne(id, {
       relations: ['order_products', 'customer'],
     });
+
+    return order;
+  }
+  public async createOrder({ customer, products }: IRequest): Promise<Order> {
+    const order = this.create({
+      customer,
+      order_products: products,
+    });
+
+    await this.save(order);
 
     return order;
   }
