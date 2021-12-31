5628c7f6722ef9ad51b8358e0e9eb244820fa3cd
devAndre-Isaac <andreisaac344@gmail.com>
2021-12-31 20:49:50 -0300

    feat: :sparkles: add OrdersRepository findById method

---
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Order from '../entities/Order';
+
+@EntityRepository(Order)
+export class OrdersRepository extends Repository<Order> {
+  public async findById(id: string): Promise<Order | undefined> {
+    const order = this.findOne(id, {
+      relations: ['order_products', 'customer'],
+    });
+
+    return order;
+  }
+}
