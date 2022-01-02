87e6d43d6794551d2bdd64b75e818a558da0ee3a
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-02 20:26:31 -0300

    fix: :bug: insert type in auth

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -23,7 +23,7 @@ export default function isAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decodeToken = verify(token, authConfig.jwt.secret);
+    const decodeToken = verify(token, authConfig.jwt.secret as any);
 
     const { sub } = decodeToken as ITokenPayload;
 
