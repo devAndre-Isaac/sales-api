594e1e8b2a4890c49c53ff5d2e96ca7b29aa827c
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-04 21:50:20 -0300

    feat: :sparkles: add paginate in ListUserService

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -2,13 +2,24 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: User[];
+}
+
 class ListUserService {
-  public async execute(): Promise<User[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const users = usersRepository.find();
+    const users = await usersRepository.createQueryBuilder().paginate();
 
-    return users;
+    return users as IPaginateCustomer;
   }
 }
 
