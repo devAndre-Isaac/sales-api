343b372582cd763c90d9838ca8e033ff7be4745e
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-14 17:24:56 -0300

    refactor: :recycle: update type

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -3,15 +3,14 @@ import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 import RedisCache from '@shared/cache/RedisCache';
-
-interface IRequest {
-  name: string;
-  price: number;
-  quantity: number;
-}
+import { IRequestCreate } from '../interfaces/products';
 
 class CreateProductService {
-  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+  public async execute({
+    name,
+    price,
+    quantity,
+  }: IRequestCreate): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
     const productExists = await productsRepository.findByName(name);
     if (productExists) {
