e0b0b2734d3159ab5130ff2b5f353aea12184b0f
devAndre-Isaac <andreisaac344@gmail.com>
2022-01-14 17:24:50 -0300

    refactor: :recycle: update type

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,14 +1,11 @@
 import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
+import { IRequestDelete } from '../interfaces/products';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
-interface IRequest {
-  id: string;
-}
-
 class DeleteProductService {
-  public async execute({ id }: IRequest): Promise<void> {
+  public async execute({ id }: IRequestDelete): Promise<void> {
     const productsRepository = getCustomRepository(ProductRepository);
 
     const product = await productsRepository.findOne(id);
